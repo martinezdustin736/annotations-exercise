4aa946f207344449b2c73441d124d63bc385a13d
Dustin Martinez <martinezdustin736@gmail.com>
2021-06-08 13:43:28 -0500

    added abstract

--- src/Author.java
+++ src/Author.java
@@ -4,7 +4,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("all")
+
 
 public class Author extends Person {
     private List books;
@@ -30,7 +30,7 @@ public class Author extends Person {
         books.add(book);
     }
 
-    //@Override
+    @Override
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
